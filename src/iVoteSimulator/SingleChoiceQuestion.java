1ebaa96343ad25e2dad3fc37fdc0762be1c186ab
Catherine Gronkiewicz <35318088+catherinegronkiewicz@users.noreply.github.com>
2020-09-28 11:54:28 -0700

    Update SingleChoiceQuestion.java
    updated initial commit - format issues

--- src/iVoteSimulator/SingleChoiceQuestion.java
+++ src/iVoteSimulator/SingleChoiceQuestion.java
@@ -11,29 +11,28 @@ import java.util.ArrayList;
 
 public class SingleChoiceQuestion implements Question {
 
-	// either multiple choice or single choice question
-	private String question;
+    // either multiple choice or single choice question
+    private String question;
 	
-	// stores multiple choice responses
-	private ArrayList<String> studentResponse;
+    // stores multiple choice responses
+    private ArrayList<String> studentResponse;
 	
-	public SingleChoiceQuestion(String scQuestion, ArrayList<String> response)
-	{
-		this.question = scQuestion;
-		this.studentResponse = response;
-	}
+    public SingleChoiceQuestion(String scQuestion, ArrayList<String> response) {
+	this.question = scQuestion;
+	this.studentResponse = response;
+    }
 	
-	public String getQuestion() {
-		return question;
-	}
+    public String getQuestion() {
+	return question;
+    }
 	
-	// returns single choice responses in array list
-	public ArrayList<String> getResponse() {
-		return studentResponse;
-	}
+    // returns single choice responses in array list
+    public ArrayList<String> getResponse() {
+	return studentResponse;
+    }
 	
-	// condition if question is multiple choice (false)
-	public boolean isMultipleChoice() {
-		return false;
-	}
+    // condition if question is multiple choice (false)
+    public boolean isMultipleChoice() {
+	return false;
+    }
 }
