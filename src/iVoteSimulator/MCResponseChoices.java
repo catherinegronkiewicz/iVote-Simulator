c0ee75ca39767e250d3b957ba144d59e98668791
Catherine Gronkiewicz <35318088+catherinegronkiewicz@users.noreply.github.com>
2020-09-28 11:40:50 -0700

    Update MCResponseChoices.java
    updated initial commit - format issues

--- src/iVoteSimulator/MCResponseChoices.java
+++ src/iVoteSimulator/MCResponseChoices.java
@@ -9,21 +9,21 @@ package iVoteSimulator;
 
 public class MCResponseChoices implements ResponseChoices {
 
-	// variables for multiple question choices 
-	private String aChoice = "Red";
-	private String bChoice = "Orange";
-	private String cChoice = "Yellow";
-	private String dChoice = "Green";
-	private String eChoice = "Blue";
+    // variables for multiple question choices 
+    private String aChoice = "Red";
+    private String bChoice = "Orange";
+    private String cChoice = "Yellow";
+    private String dChoice = "Green";
+    private String eChoice = "Blue";
 	
-	// prints out multiple choice question options
-	public String getResponseChoices() {	
-		String scQuestion = "What are your favorite colors?";
-		String choice = "\nA: " + aChoice 
-					  	+ "\nB: " + bChoice
-					  	+ "\nC: " + cChoice
-					  	+ "\nD: " + dChoice
-					  	+ "\nE: " + eChoice;
-		return scQuestion + choice;
-	}
+    // prints out multiple choice question options
+    public String getResponseChoices() {	
+	String scQuestion = "What are your favorite colors?";
+	String choice = "\nA: " + aChoice 
+			+ "\nB: " + bChoice
+			+ "\nC: " + cChoice
+			+ "\nD: " + dChoice
+			+ "\nE: " + eChoice;
+	return scQuestion + choice;
+    }
 }
